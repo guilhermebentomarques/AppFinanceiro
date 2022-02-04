bbba7a16947e455535396fdb86ae1d291f6ddcb6
guilherme.marques <guilherme.marques@photoweb.fr>
2022-02-04 23:45:16 +0000

    first commit

---
+++ aspnet-core/src/MyProject.Core/MyProjectConsts.cs
@@ -0,0 +1,20 @@
+﻿using MyProject.Debugging;
+
+namespace MyProject
+{
+    public class MyProjectConsts
+    {
+        public const string LocalizationSourceName = "MyProject";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "b9a0b861e15647519737d71db2f89fb0";
+    }
+}
